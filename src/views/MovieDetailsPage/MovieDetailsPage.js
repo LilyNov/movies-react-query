2ce682c8d38fc4c2acb9db44bd2b911d496a999f
Liliia Novikova <kplily85@gmail.com>
2021-01-08 19:18:19 +0200

    add netlify

--- src/views/MovieDetailsPage/MovieDetailsPage.js
+++ src/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,64 +1,52 @@
+import { useQuery } from "react-query";
 import {
   Route,
   useParams,
   useRouteMatch,
   NavLink,
   useLocation,
-} from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { animateScroll as scroll } from 'react-scroll';
-
-import { useState, useEffect } from 'react';
-import * as moviesAPI from '../../service/home-app';
-import StatusError from '../../StatusError/StatusError';
-import CardOfMovie from '../CardOfMovie/CardOfMovie';
-import Cast from '../Cast/Cast';
-import Reviews from '../Reviews/Reviews';
-import s from '../MovieDetailsPage/MovieDetailsPage.module.css';
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { animateScroll as scroll } from "react-scroll";
+import { KEY, BASE_URL } from "../../service/home-app";
+import StatusError from "../../StatusError/StatusError";
+import CardOfMovie from "../CardOfMovie/CardOfMovie";
+import Cast from "../Cast/Cast";
+import Reviews from "../Reviews/Reviews";
+import s from "../MovieDetailsPage/MovieDetailsPage.module.css";
 
 export default function MovieDetailsPage() {
   const queryClient = new QueryClient();
 
   const location = useLocation();
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
   const { url, path } = useRouteMatch();
   const { slug } = useParams();
   const movieId = slug.match(/[a-zA-Z0-9]+$/)[0];
-  const movieImg = `https://image.tmdb.org/t/p/w500/${movies.backdrop_path}`;
-
-  useEffect(() => {
-    setStatus('pending');
-
-    moviesAPI
-      .fetchMoviesInfo(movieId)
-      .then(newMovies => {
-        setMovies(newMovies);
-        setStatus('resolved');
-      })
-      .catch(error => {
-        setError(error);
-        setStatus('rejected');
-      });
-  }, [movieId]);
+  const { data, status } = useQuery("movieDetailsPage", () =>
+    fetch(`${BASE_URL}/movie/${movieId}?api_key=${KEY}`).then((res) =>
+      res.json()
+    )
+  );
 
   return (
     <div>
-      {status === 'rejected' && (
-        <StatusError message={error.message} style={{ textAlign: 'center' }} />
+      {status === "error" && (
+        <StatusError
+          message={"not found anything"}
+          style={{ textAlign: "center" }}
+        />
       )}
-      {status === 'resolved' && (
+      {status === "success" && (
         <>
           <CardOfMovie
-            title={movies.title}
+            title={data.title}
             image={
-              movies.backdrop_path !== null
-                ? movieImg
-                : 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Foto'
+              data.backdrop_path !== null
+                ? `https://image.tmdb.org/t/p/w500/${data.backdrop_path}`
+                : "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Foto"
             }
-            overview={movies.overview}
-            part={movies.genres.map(movie => movie.name)}
+            overview={data.overview}
+            part={data?.genres?.map((movie) => movie.name)}
           />
           <section className={s.about}>
             <NavLink
